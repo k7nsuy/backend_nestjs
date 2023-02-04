ab3fa651f55e447bf7e536a84f5c4394d3e1575f
Sangwoo Yu <76999175+k7nsuy@users.noreply.github.com>
2023-02-04 15:33:57 +0900

    commit

---
+++ 06-02-rest-api copy/backend/phone.js
@@ -0,0 +1,45 @@
+import coolsms from 'coolsms-node-sdk'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export function checkThePhoneNumber(phoneNumber) {
+    if(phoneNumber.length !== 10 && phoneNumber.length !== 11) {
+        console.log('Error - Check the phone number');
+    } else {
+        return phoneNumber
+    }
+}
+
+export function getToken() {
+    const tokenNumber = 6
+    if(tokenNumber === undefined) {
+        console.log('Error');
+        return
+    } else if(tokenNumber <= 0) {
+        console.log('Error - ');
+        return
+    } else if(tokenNumber > 10) {
+        console.log('Error - too many counts');
+    }
+    // create token as randomly numbers
+    const result = String(Math.floor(Math.random() * (10 ** tokenNumber))).padStart(tokenNumber, '0')
+    return result
+}
+
+export async function sendTokenToSMS(phoneNumber,myToken) {
+    const SMS_API_Key = process.env.SMS_API_KEY
+    const SMS_API_Secret = process.env.SMS_API_SECRET
+    const SMS_Sender = process.env.SMS_SENDER
+
+    const mySMS = coolsms.default
+    const message_Service = new mySMS(SMS_API_Key,SMS_API_Secret)
+    const result = await message_Service.sendOne({
+        to: phoneNumber,
+        from: SMS_Sender,
+        text: `인증하신 번호는 [${myToken}] 입니다.`
+    })
+    
+    console.log(result);
+    return result
+}
